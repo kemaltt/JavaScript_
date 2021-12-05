28351f4e8d99818772c10bc85cd1440f0a11732b
Kemal <ttnckemal@gmail.com>
2021-12-05 22:09:54 +0100

    Function

--- index.js
+++ index.js
@@ -160,22 +160,22 @@
 
 // Template Literal--------------------------
 
-let fName = "Jeff"
-let sName = "Schneider"
-let work = "Amozon"
+// let fName = "Jeff"
+// let sName = "Schneider"
+// let work = "Amozon"
 
-let sentence = "He is " + fName + ' ' + sName + ' and He is founder of ' + work + '.';
+// let sentence = "He is " + fName + ' ' + sName + ' and He is founder of ' + work + '.';
 
-console.log(sentence);
+// console.log(sentence);
 
-let betterSentence = `He is ${fName} ${sName} and He is founder of ${work}.`
-console.log(betterSentence);
+// let betterSentence = `He is ${fName} ${sName} and He is founder of ${work}.`
+// console.log(betterSentence);
 
-console.log("I want to write this \n\
-on new line")
+// console.log("I want to write this \n\
+// on new line")
 
-console.log(`I want to write this \n\
-on new line`)
+// console.log(`I want to write this \n\
+// on new line`)
 
 
 // Conditionals--------------------------------
@@ -201,74 +201,74 @@ on new line`)
 
 // yada-----------
 
-const birthYear = 2001;
+// const birthYear = 2001;
 
-let generation;
+// let generation;
 
-if (birthYear >= 2000) {
-    generation = "Z"
+// if (birthYear >= 2000) {
+//     generation = "Z"
 
-} else if (birthYear < 2000) {
-    generation = "Y"
+// } else if (birthYear < 2000) {
+//     generation = "Y"
 
-}
+// }
 
-console.log(generation);
+// console.log(generation);
 
 // -----------------------------------
 
-let grade = 80
+// let grade = 80
 
-if (grade >= 90) {
+// if (grade >= 90) {
 
-    let not = "AA"
+//     let not = "AA"
 
-    console.log(not);
+//     console.log(not);
 
-} else if (grade <= 80) {
+// } else if (grade <= 80) {
 
-    let not = "BA"
-    console.log(not);
-} else {
+//     let not = "BA"
+//     console.log(not);
+// } else {
 
-    let not = "BB"
-    console.log(not);
-}
+//     let not = "BB"
+//     console.log(not);
+// }
 
 // TYPE CONVERSION AND TYPE COERCION-----------------------------
 
-console.log(typeof "18");
-console.log(typeof Number("18"));
-console.log(typeof String(18));
+// console.log(typeof "18");
+// console.log(typeof Number("18"));
+// console.log(typeof String(18));
 
-console.log(Number("Gamze")); // NaN yazar
+// console.log(Number("Gamze")); // NaN yazar
 
-let num = "5"
+// let num = "5"
 
-let mul = "5" * 2
-console.log(mul); // 10 yazar
+// let mul = "5" * 2
+// console.log(mul); // 10 yazar
 
-let divide = 10 / num
+// let divide = 10 / num
 
-console.log(divide); // 2 yazar
+// console.log(divide); // 2 yazar
 
-let minus = num - 1;
+// let minus = num - 1;
 
-console.log(minus); // 4 yazar
+// console.log(minus); // 4 yazar
 
-let add = num + 5;
+// let add = num + 5;
 
-console.log(add); // 55 string olarak yazar  burada dönüşüm yapmalıyız
+// console.log(add); // 55 string olarak yazar  burada dönüşüm yapmalıyız
 
-let add1 = 5 + Number(num);
-console.log(add1); // 10 yazar
+// let add1 = 5 + Number(num);
+// console.log(add1); // 10 yazar
 
 
-let a = "1"
-a += 1 // 11 string yazar
-a -= 1 // 10 number
+// let a = "1"
+// a += 1 // 11 string yazar
+// a -= 1 // 10 number
 
-console.log(a);
+// console.log(a);
 
 
 
@@ -285,11 +285,11 @@ console.log(a);
  * Nan
  */
 
-let cash = 0;
+// let cash = 0;
 
-if (cash) {
-    console.log("Find a job")
-}
+// if (cash) {
+//     console.log("Find a job")
+// }
 // If you write one line code u don't have to use curly braces
 // if(cash == 0) console.log("No jobs found")
 
@@ -406,12 +406,362 @@ console.log("season: ", season)
 // NULLISH COALESCING
 
 //const val = "xyz"
-const val = undefined
+// const val = undefined
 
-const value = val ? ? "Ahmet"
+// const value = val ? ? "Ahmet"
 
-console.log("values ", value)
+// console.log("values ", value)
 
-console.log(0 || "Ahmet")
+// console.log(0 || "Ahmet")
 
-console.log("Ahmet" && undefined && 2)
+// console.log("Ahmet" && undefined && 2)
+
+// Ödev-------------------------------------------
+
+/*
+There are two gymnastics teams, Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins a trophy!
+Your tasks:
+1. Calculate the average score for each team,using the test data below
+Compare the team's average scores to determine the winner of the competition,
+and print it to the console. 
+2. Include a requirement for a minimum scoreof 100. Witht his rule,a
+team only wins if it has a higher score than the other team, and the same time a score of at least 100 points. 
+3. Minimum score also applies to a draw!So a draw only happens when both teams have the same score and both have a score greater or equal 100 points. Otherwise, no team wins the trophy
+Test data:
+§ Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+§ Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123 
+§ Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+*/
+
+
+// 1-§ Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110-----------------------------------
+// let comAvrdolp, comAvrkoal, scoreDolp1, scoreDolp2, scoreDolp3, scoreKoal1, scoreKoal2, scoreKoal3, avrDolp, avrKoal;
+
+// scoreDolp1 = 96;
+// scoreDolp2 = 108;
+// scoreDolp3 = 89;
+
+// avrDolp = (scoreDolp1 + scoreDolp2 + scoreDolp3) / 3;
+
+// comAvrdolp = avrDolp.toFixed(2);
+
+// scoreKoal1 = 88;
+// scoreKoal2 = 91;
+// scoreKoal3 = 110;
+
+// avrKoal = (scoreKoal1 + scoreKoal2 + scoreKoal3) / 3;
+
+// comAvrkoal = avrKoal.toFixed(2);
+
+// console.log(`Dolphins avarage:${comAvrdolp}
+// Koalas avarage:${comAvrkoal}`);
+
+//  Dolphins avarage:97.67
+// Koalas avarage:96.33
+
+// 2-§ Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123--------------------------------------
+// let scoreDolp1, scoreDolp2, scoreDolp3, scoreKoal1, scoreKoal2, scoreKoal3, avrDolp, avrKoal;
+
+// scoreDolp1 = 97;
+// scoreDolp2 = 112;
+// scoreDolp3 = 101;
+
+// avrDolp = (scoreDolp1 + scoreDolp2 + scoreDolp3) / 3;
+// comAvrdolp = avrDolp.toFixed(1);
+
+
+
+// scoreKoal1 = 109;
+// scoreKoal2 = 95;
+// scoreKoal3 = 123;
+
+// avrKoal = (scoreKoal1 + scoreKoal2 + scoreKoal3) / 3;
+// comAvrkoal = avrKoal.toFixed(1);
+
+// if (avrDolp > avrKoal && avrDolp >= 100) {
+//     console.log(`The winning Dolphin team!, Score:${comAvrdolp}`);
+// } else if (avrKoal > avrDolp && avrKoal >= 100) {
+//     console.log(`The winning Koala team!, Score:${comAvrkoal}`);
+
+// } else {
+//     console.log("No winner!")
+// }
+
+// The winning Koala team!, Score:109.0
+
+
+
+// 3-§ Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106------------------------
+
+// let scoreDolp1, scoreDolp2, scoreDolp3, scoreKoal1, scoreKoal2, scoreKoal3, avrDolp, avrKoal;
+
+// scoreDolp1 = 97;
+// scoreDolp2 = 112;
+// scoreDolp3 = 101;
+
+// avrDolp = (scoreDolp1 + scoreDolp2 + scoreDolp3) / 3;
+
+
+
+// scoreKoal1 = 109;
+// scoreKoal2 = 95;
+// scoreKoal3 = 106;
+
+// avrKoal = (scoreKoal1 + scoreKoal2 + scoreKoal3) / 3;
+
+// if (avrDolp == avrKoal && avrDolp >= 100 && avrKoal >= 100) {
+//     console.log(`This is draw!`)
+
+// } else {
+//     console.log('no winner!')
+// }
+
+// Dolphin and Koala winner!
+
+// ---------------------------------------------------------------------------------------------------
+
+
+// Take inputs from the user (the day and the time)  and return output whether is open or closed.
+// Open -> Monday to Friday, 10-21 
+// Close -> all weekend
+
+// const tag = prompt("Geben Sie den Tag");
+
+// const uhr = prompt("Geben Sie die Uhr");
+
+// if (tag != "Montag" && tag != "Dienstag" && tag != "Mittwoch" && tag != "Donnerstag" && tag != "Freitag" && tag != "Samstag" && tag != "Sonntag") {
+//     alert('Geben Sie richtigen Tagsname')
+// }
+
+// if (uhr >= 10 && uhr <= 22) {
+//     switch (tag) {
+//         case ("Montag"):
+//             console.log("von 10 Uhr bis 22 Uhr geöffnet")
+//             break
+//         case ("Dienstag"):
+//             console.log("von 10 Uhr bis 22 Uhr geöffnet")
+//             break
+//         case ("Mittwoch"):
+//             console.log("von 10 Uhr bis 22 Uhr geöffnet")
+//             break
+//         case ("Donnerstag"):
+//             console.log("von 10 Uhr bis 22 Uhr geöffnet")
+//             break
+//         case ("Freitag"):
+//             console.log("von 10 Uhr bis 22 Uhr geöffnet")
+//             break
+
+//         default:
+//             console.log("Wochenende Geschlossen")
+//             break
+//             // case "Samstag":
+//             // case "Sonntag":
+//             //     console.log("Wochenende Geschlossen")
+//             //     break
+//     }
+
+// } else {
+//     console.log("Geschlossen")
+// }
+
+
+// // Q1
+
+// const avgDolphins = (96 + 108 + 89) / 3
+// const avgKoalas = (88+ 91 + 110) / 3
+
+// console.log(avgDolphins, avgKoalas)
+
+// if (avgDolphins > avgKoalas){
+//     console.log("Winner is Dolphins")
+// }else {
+//     console.log("Winner Kolas")
+// }
+
+// // Q2
+
+// const avgDolphins2 = (97 + 112 + 101) / 3
+// const avgKoalas2 = (109+ 95 + 123) / 3
+
+// console.log(avgDolphins2, avgKoalas2)
+
+// if(avgDolphins2 > avgKoalas2 &&  avgDolphins2 >= 100){
+//     console.log("Winner is Dolphins")
+
+// }else if(avgKoalas2 > avgDolphins2 && avgKoalas2 >=100){
+//     console.log("Winner Koalas")
+// }
+
+// // Q3 
+
+// const avgDolphins3 = (97 + 112 + 101) / 3
+// const avgKoalas3 = (109+ 95 + 106) / 3
+
+// console.log(avgDolphins3, avgKoalas3)
+
+// if(avgDolphins3 > avgKoalas3 &&  avgDolphins3 >= 100){
+//     console.log("Winner is Dolphins")
+
+// }else if(avgKoalas3 > avgDolphins3 && avgKoalas3 >=100){
+//     console.log("Winner Koalas")
+// }else if (avgKoalas3 == avgDolphins3 && avgKoalas3 >= 100 && avgDolphins3 >= 100){
+//     console.log("This is draw")
+// }else {
+//     console.log("There is no winner")
+// }
+
+
+// // Take inputs from the user (the day and the time)  and return output whether is open or closed.
+// // Open -> Monday to Friday, 10-21 
+// // Close -> all weekend
+
+// const day = prompt("Enter the day: ")
+// const time = prompt ("Enter the time: ")
+
+// if (day != "Monday" && day != "Tuesday" && day!= "Wednesday" && day !="Thursday" && day != "Friday" && day != "Saturday" &&  day != "Sunday"){
+//     alert("Please enter valid day")
+// }else {
+//     if ( (day != "Saturday" && day != "Sunday") &&  (time >= 10 && time < 21) ){
+//         console.log("Open")
+//     }else{
+//         console.log("Close")
+//     }
+// }
+
+
+
+
+// if (hour <=21 && hour >= 10) {
+//     switch (day) {
+//         case ("monday"):
+//             console.log("OPEN!")
+//             break
+//         case ("tuesday"):
+//             console.log("OPEN!")
+//             break
+//         case ("wednesday"):
+//             console.log("OPEN!")
+//             break
+//         case ("thursday"):
+//             console.log("OPEN!")
+//             break
+//         case ("friday"):
+//             console.log("OPEN!")
+//             break
+//         default:
+//             console.log("CLOSED!")
+//             break
+//     }
+// }
+// else{
+//     console.log("CLOSED!")
+// }
+
+
+// if(gun=="Monday" && time>=10 && time<=21)   { console.log("open")}
+// else if(gun=="Tuesday" && time>=10 && time<=21)  { console.log("open")}
+// else if(gun=="Wednesday" && time>=10 && time<=21){ console.log("open")}
+// else if(gun=="Thursday" && time>=10 && time<=21) { console.log("open")}
+// else if(gun=="Friday" && time>=10 && time<=21)   { console.log("open")}
+// else                                             {console.log("close")} 
+
+// ---------FUNCTION---------------------------------------------------
+
+function myFirstFunction(a) {
+    console.log("Hello World")
+}
+
+myFirstFunction(); // call,invoke,execute
+
+
+// function juiceExtractor(fruit) {
+//     const juice = `This is ${fruit} juice`
+//     console.log(juice);
+// }
+
+// juiceExtractor("apple")
+
+// function juiceExtractor(fruit1, fruit2) {
+//     const juice = `This is ${fruit1} and ${fruit2} juice.`
+//     console.log(juice);
+//     return juice
+// }
+
+// juiceExtractor("apple", "orange")
+
+//---- Function decleration 
+function calcAge(birthYear) {
+    // const age = 2021 - birthYear
+    // return age
+    return 2021 - birthYear
+}
+
+//console.log(calcAge(1965))
+
+// console.log(calcAge(1997))
+
+//---- Function Expression code
+const addition = function(a, b) {
+    return a + b
+}
+
+const total = addition(1965, 1612737)
+console.log(total)
+
+
+// ---- Arrow Function 
+
+const calcAge2 = birthYear => 2021 - birthYear
+
+console.log(calcAge2(2000))
+
+const canDrive = (birthYear) => {
+    const age = 2021 - birthYear
+    const allowed = 18 - age
+    return `You can drive ${allowed} year later.`
+}
+
+const result = canDrive(2006)
+console.log(result)
+
+
+
+function hipotenus(a, b) {
+    const firstValue = exponentValue(a)
+    const secondValue = exponentValue(b)
+    const result = (firstValue + secondValue) ** (0.5)
+    return result
+}
+
+function exponentValue(value) {
+    return value ** 2
+}
+
+const hipoResult = hipotenus(7, 24)
+console.log(hipoResult)
+
+// -----------------------------------------------
+
+function average(a, b, c) {
+
+    return (a + b + c) / 3
+
+}
+
+const avgDolphin = average(96, 108, 89);
+console.log(avgDolphin);
+
+const avgKoala = average(88, 91, 110);
+console.log(avgKoala);
+
+function findWinner(avgDolphin, avgKoala) {
+
+    if (avgDolphin > avgKoala) {
+        return `The winner is Dolphin`
+    } else {
+        return `The winner is Koala`
+    }
+
+}
+
+console.log(findWinner(avgDolphin, avgKoala));
