caada6d58c841105c481006eb2c934c22355e2c5
Kemal <ttnckemal@gmail.com>
2021-12-19 21:10:52 +0100

    EVENT

---
+++ jsEVENT/index.js
@@ -0,0 +1,11 @@
+// const colorChangeFunction = () => {
+//     let button = document.querySelector('#colorChangeButton')
+// }
+
+
+
+const colorChangeFunction = (element, background, text) => {
+    element.style.backgroundColor = background;
+    element.style.color = text;
+
+}
